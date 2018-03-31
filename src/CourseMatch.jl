e1114ee68ceb24ce4fffb7cee920b2ced2ac19c1
florian oswald <florian.oswald@gmail.com>
2018-03-31 23:57:31 +0200

    best bundel

--- src/CourseMatch.jl
+++ src/CourseMatch.jl
@@ -1,7 +1,38 @@
 module CourseMatch
 
 	VERSION = VersionNumber(0,0,0)
+	using JuMP, Cbc
 
-# package code goes here
+
+	function best_bundle(;m=10,b=2)
+		srand(1)
+		# m = courses available
+		k = 5 # courses to take
+		price = collect(linspace(0.1,1,m))
+		prefs = collect(linspace(0.1,1,m))
+
+		println("price = $price")
+		println("prefs = $prefs")
+
+		model = Model(solver=CbcSolver())
+		@variable(model, x[1:m] >= 0, Bin)  
+
+		# objective: sum of preferences
+		@objective(model, Max, dot(prefs,x) )
+
+		# constraint: cost is less than budget
+		@constraint(model, dot(price,x) <= b )
+
+		# can only choose k courses out of all m
+		@constraint(model, sum(x) <= k )
+
+		status =solve(model)
+		println("Objective is: ", getobjectivevalue(model))
+		println("Solution is:")
+		println("find(x) = $(find(getvalue(x)))")
+		println("total cost = $(dot(price,getvalue(x)))")
+
+
+	end
 
 end # module
