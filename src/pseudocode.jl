8d41727b333ef7a4ade71a0e83bdd2176e0145fd
Marie <marie.cases@sciencespo.fr>
2018-04-05 16:50:11 +0200

    Created a tree to input pseudo code

---
+++ src/pseudocode.jl
@@ -0,0 +1,21 @@
+#Inputs just to test loops
+    #Number of courses
+    M = 5
+    βmax = 20
+    #d(p) stupid demand function takes price vector of size M and returns demand vector size M# - Issue #4 to get the full function
+    #N(p) creates set of neighbors. WE DONT UNDERSTAND THIS - Issue #6/1
+    #t is overall time - performance
+    # l.1
+
+
+#Code to fill out
+
+# repeat from l.2 to 35
+    τ =    #empty
+    c=0
+    # while loop from l.7 to 34
+        # repeat from l. 10 to 16
+            # if from l.13 to 15
+        # if from line 17 to 33
+            # if from l.23 to 28
+            # if from l.29 to 32
