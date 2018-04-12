f3d5fc733f7bb940f1962bb76d5c7cf3ab5d6809
florian oswald <florian.oswald@gmail.com>
2018-04-12 17:17:09 +0200

    one feasibility constraint matrix

--- src/CourseMatch.jl
+++ src/CourseMatch.jl
@@ -1,20 +1,24 @@
 module CourseMatch
 
 	VERSION = VersionNumber(0,0,0)
-	using JuMP, Cbc
+	using JuMP, Gurobi
 
 
-	function best_bundle(;m=10,b=2)
+	function best_bundle(;k=3,m=10,b=2)
 		srand(1)
 		# m = courses available
-		k = 5 # courses to take
+
 		price = collect(linspace(0.1,1,m))
 		prefs = collect(linspace(0.1,1,m))
 
 		println("price = $price")
 		println("prefs = $prefs")
 
-		model = Model(solver=CbcSolver())
+		c=zeros(m,m)
+		c[1,2] = 1
+		c[1,8] = 1
+
+		model = Model(solver=GurobiSolver())
 		@variable(model, x[1:m] >= 0, Bin)  
 
 		# objective: sum of preferences
@@ -25,6 +29,10 @@ module CourseMatch
 
 		# can only choose k courses out of all m
 		@constraint(model, sum(x) <= k )
+
+		# # cannot choose infeasible courses
+		@constraint(model, vecdot(x'*c, x) == 0)
+			 
 
 		status =solve(model)
 		println("Objective is: ", getobjectivevalue(model))
