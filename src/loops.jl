77be21012b0a490ff646ab62029b87cddfa2f02b
Marie <marie.cases@sciencespo.fr>
2018-04-30 15:54:13 +0200

    Cleaned up pseudo code

--- src/loops.jl
+++ src/loops.jl
@@ -11,31 +11,22 @@
     function checktabu() #if from l.13 to 15
         if (dem in τ) == false #If dem is not in the tabu list
             global foundnextstep = true
-            println(foundnextstep)
-        else #Marie, remember to remove this eventually. It's useful to check that the code is making it to this step.
-            println("No next step")
         end
     end
 
     #newsetup generates a new search using the price vector N1 if it was not in the tabu list. This also involes adding d(N1) to the tabu list.
     function newsetup()
         global p = ptild
-        println(p)
         push!(τ,dem)
-        #global currenterror = α.(dem)
     end
 
     #replacesearcherror will check is the current error is less than the best error in the search start (searcherror). If it is, then currenterror becomes the new searcherror. Since we've improved on our searcherror, the step counter starts again. If we haven't imroved on our search error, the step counter increases by 1.
     function replacesearcherror()
         if currenterror[1] < searcherror[1]
             global searcherror = currenterror
-            println("New searcherror = $searcherror")
             global c = 0
-            println(c)
         else
             global c = c + 1
-            println("No new searcherror")
-            println(c)
         end
     end
 
@@ -43,7 +34,6 @@
     function replacebesterror()
         if currenterror[1] < besterror[1]
             global besterror = currenterror
-            println("New besterror is $besterror")
             global pstar = p
         end
     end
