a3e2a10d05cdede6ce7041577716457ac214b845
Gegas2231 <gegas2231@gmail.com>
2018-05-02 02:45:41 +0200

    Added time clashes, mandatory courses, program and smeester constraints Changed the structure of the function so the iteration over all students occurs outside the function the test scenario is not working, optimization is infeasible, but the algorithm should work with propper data

--- src/Issue#4-demandfunc.jl
+++ src/Issue#4-demandfunc.jl
@@ -10,73 +10,101 @@ The argument to the *demand* function defined above should be such that :
 
 	-> *budget* is a column vector of dimension N x 1, which n-th element is the budget that was allocated to the n-th student.
 
-	-> *capacity* is a column vector of dimension N x 1, which n-th element is the number of classes student n has to attend. =#
+	-> *capacity* is a column vector of dimension N x 1, which n-th element is the number of classes student n has to attend.
 
-function demand(price, pref, budget, capacity)
+	-> *time_const* is an sparse array C x C that flags with 1 if the ith element HAS a clash with jth element. 0 otherwise.
 
-	demand = []
+	-> *mand_const* is a vector C x 1. the i-th element is 1 if the course must be taken, 0 otherwise
 
-	M = length(pref)
+	-> *prog_cour_const* is a vector C x 1. the i-th element is 1 if the course is NOT in the students's program. 0 otherwise.
 
-	for i in 1:M
+	-> *sem_cour_const* is a vector C x 1. the i-th element is 1 if the course is NOT the students's semester =#
 
-		N = size(Ind_pref[i])[1]
+function demand(price, pref, budget, capacity, time_const, mand_const, prog_cour_const, sem_cour_const)
 
-		let
-			# Maximization problem
-			m = Model(solver=GurobiSolver())
+	dem = []
 
-			@variable(m, x[1:N], Bin)
+	N = size(pref)[1]
 
-			# Objective: maximize utility
-			@objective(m, Max, x'*pref[i]*x)
+	let
+		# Maximization problem
+		m = Model(solver=GurobiSolver())
 
-			# Constraints:
+		@variable(m, x[1:N], Bin)
 
-			#Should have exactly 3 classes
-			@constraint(m, sum(x) == capacity[i])
+		# Objective: maximize utility
+		@objective(m, Max, x'*pref*x)
 
-			#Should not spend more than one's budget
-			@constraint(m,  dot(price, x) <= budget[i])
+		# Constraints:
 
-			# Solve problem using MIP solver
-			status = solve(m)
-			push!(demand, getvalue(x))
-		end
+		#Should have exactly 3 classes
+		@constraint(m, sum(x) == capacity)
+
+		#Should not spend more than one's budget
+		@constraint(m,  dot(price, x) <= budget )
+
+		#Time Constraints
+		@constraint(m, x'*time_const*x == 0 )
+
+		#Mandatory courses
+		@constraint(m, mand_const'*x == sum(mand_const) )
+
+		#Program courses
+		@constraint(m, prog_cour_const'*x == 0 )
+
+		#Program courses
+		@constraint(m, sem_cour_const'*x == 0 )
+
+		# Solve problem using MIP solver
+		status = solve(m)
+		dem = getvalue(x)
+		return dem
 
 	end
-
-	return Dict("ind_demands" => demand,
-					"total_demand" => sum(demand) )
 
 end
 
 
 ######
 
-## Example : 20 individuals, 10 classes to choose from, exactly 3 classes to attend, NO CROSS PREFERENCES
+M = 20
 
-# Individual preferences are generated as an array of sparse diagonal matrices
+for i in 1:M
 
-Ind_pref = []
-for i in 1:20
-	push!(Ind_pref, sparse(collect(1:10), collect(1:10), rand(0:100, 10)))
+	# Individual preferences are generated as an array of sparse diagonal matrices
+	Ind_pref = sparse(collect(1:10), collect(1:10), rand(0:100, 10))
+
+	# Individual budget
+	ind_budget = rand(150:200)
+
+	# Price vector
+	price = rand(Float32, 10)
+
+	# Capacity vector
+	cap = rand(3:5)
+
+	#Schedule collisions
+	time_const = zeros(10,10)
+
+	#Mandatory courses
+	mand_const = rand(0:1,10)
+
+	#Program courses
+	prog_cour_const = 1 - mand_const
+
+	#Semester courses
+	sem_cour_const = prog_cour_const
+
+	#Demand computation
+	dem = demand(price, Ind_pref, ind_budget, cap, time_const, mand_const, prog_cour_const, sem_cour_const)
+
+	mkt_demand = []
+	push!(mkt_demand, dem)
+	return mkt_demand
 end
 
-# Individual budget
+mkt_demand = Dict("ind_demands" => ans, "total_demand" => sum(ans) )
 
-ind_budget = rand(150:200, 20)
 
-# Price vector
-
-price = rand(Float32, 10)
-
-# Capacity vector
-
-cap = fill(3, 20)
-
-# Function application
-
-demand(price, Ind_pref, ind_budget, cap)
 
 ######
