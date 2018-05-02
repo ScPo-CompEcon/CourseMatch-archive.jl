18efb58cc85425c71d49532921c181fcd6b1b211
Marie <marie.cases@sciencespo.fr>
2018-05-02 18:08:37 +0200

    Fixed tau and got rid of globals, but still needs work on replacesearcherror and replacebesterror functions

--- src/loops.jl
+++ src/loops.jl
@@ -1,3 +1,15 @@
+    #=The function finds the next relevant price vector among the neighbors. Basically, it takes each price vector in the list of neighbors
+    and looks if the demand generated is in the tabu list. If so, it goes to the next neighor, otherwise, it has found the new price vector!
+    Its value is pushed into ptild, tried price vectors are removed from DoubleN, dem is the demand associated with this new price vector and
+    foundnextstep is changed to true if it has found a new price vector, while the tabu list is left unchanged.
+    =#
+    function findnextprices!(ptild, DoubleN, dem, foundnextstep, τ)
+        while foundnextstep[1] == false | isempty(DoubleN) == false
+            Dico = findingnextstep(DoubleN, τ)
+            ptild[:], dem[:], foundnextstep[1] = Dico["ptild"][1], Dico["dem"][1],  Dico["foundnextstep"]
+        end
+    end
+
     # Findingnextstep does the following : 1. It isolates the first neighbor (N1) of the neighbor list, removes N1 from neighbor list (such that N becomes N-1), checks the demand of N1, and checks if that demand is in the tabu list.
     function findingnextstep(DoubleN, τ)
         ptild = Array[DoubleN[1]]
@@ -21,44 +33,30 @@
     end
 
     #newsetup generates a new search using the price vector N1 if it was not in the tabu list. This also involes adding d(N1) to the tabu list.
-    function newsetup()
-        global p = ptild
-        println(p)
-        push!(τ,dem)
+    function newp(ptild)
+        p = ptild
+        Dico = Dict("p" => p)
+        return Dico
     end
 
     #replacesearcherror will check is the current error is less than the best error in the search start (searcherror). If it is, then currenterror becomes the new searcherror. Since we've improved on our searcherror, the step counter starts again. If we haven't imroved on our search error, the step counter increases by 1.
-    function replacesearcherror()
+    function replacesearcherror(currenterror, searcherror, c)
         if currenterror[1] < searcherror[1]
-            global searcherror = currenterror
-            println("New searcherror = $searcherror")
-            global c = 0
-            println(c)
+            searcherror = currenterror
+            #println("New searcherror = $searcherror")
+            c = 0
+            #println(c)
         else
-            global c = c + 1
-            println("No new searcherror")
-            println(c)
+            c = c + 1
+            #println("No new searcherror")
+            #println(c)
         end
     end
 
     #replacebesterror will check that the currenterror is less than the best error that from ALL search starts. If it is, then that currenterror becomes the best error and the associated price vector is the new best price.
-    function replacebesterror()
+    function replacebesterror(currenterror, besterror, p)
         if currenterror[1] < besterror[1]
-            global besterror = currenterror
-            global pstar = p
-        end
-    end
-
-
-    #=The function finds the next relevant price vector among the neighbors. Basically, it takes each price vector in the list of neighbors
-    and looks if the demand generated is in the tabu list. If so, it goes to the next neighor, otherwise, it has found the new price vector!
-    Its value is pushed into ptild, tried price vectors are removed from DoubleN, dem is the demand associated with this new price vector and
-    foundnextstep is changed to true if it has found a new price vector, while the tabu list is left unchanged.
-    =#
-    function findnextprices!(ptild, DoubleN, dem, foundnextstep, τ)
-        while foundnextstep[1] == false | isempty(DoubleN) == false
-            Dico = findingnextstep(DoubleN, τ)
-            ptild[:], dem[:], foundnextstep[1] = Dico["ptild"][1], Dico["dem"][1],  Dico["foundnextstep"]
-            #println(foundnextstep, ptild) #ptild = good
+            besterror[1] = currenterror[1]
+            pstar[1] = p
         end
     end
--- src/pseudocode.jl
+++ src/pseudocode.jl
@@ -1,66 +1,81 @@
 #This file provides a pseudocode for algorithm one that provides the architecture for the complete algorithm. Some of these objects can change.
 
-include("PseudoDemand.jl")
+#include("PseudoDemand.jl")
 include("PseudoClearingError.jl")
 include("PseudoNeighbor.jl")
 include("loops.jl")
 
-#Defining variables for future
-M = 5 #Number of courses offered
-k = 3 #Number of courses students take
-βmax = 20 #Maximum budget
- #Tabu list will be filled by rejected solutions.
+#Preliminary variables
+ #test neighbors
 
-#Preliminary inputs
-t = 0.005 #time in seconds
-besterror = 100 #for now
-Np = Array[[0,0,0,0,0], [1,1,1,1,1], [1,2,3,0,0]] #test neighbors
+function coursematch(M, k, βmax, besterror, t, Np)
 
-function coursematch(M, k, βmax, t)
-
-            p = Array[] #searchstart
-            ptild = Array{Int}(M) #new search price
-            pstar = Array[] #price that gives best error
-            searcherror = Array[] #best error in a searchstart
-            DoubleN = Array[] #neighbors of the search
-            foundnextstep = Array{Bool}(1) #boolean. true if a new search price has been found (one that does not generate the same demand as one that is in the tabu list)
-            dem = zeros(M) #the demand for courses at search price
-            currenterror = Array[] #error in the current search.
-            τ = Array[]
-
+    ptild = Array{Int}(M) #new search price
+    pstar = Array[] #price that gives best error
+    searcherror = Array[] #best error in a searchstart
+    DoubleN = Array[] #neighbors of the search
+    foundnextstep = Array{Bool}(1) #boolean. true if a new search price has been found (one that does not generate the same demand as one that is in the tabu list)
+    dem = zeros(M) #the demand for courses at search price
+    currenterror = Array[] #error in the current search.
+    τ = Array[]
     starttime = Dates.Time(now())
     while (Dates.Time(now()) - starttime).value < t *1000000000
     #repeat from l.2 to 35. This is a do until runtime > t.
-        global p = Array[[0,0,0,0,0]] #Initial guess for p. Will be replaced also
-        global searcherror =  α.(d(p)) ##= 50 function from issue #1
-        global c = 0
+        p = Array[[0,0,0,0,0]] #Initial guess for p. Will be replaced also
+        searcherror =  α.(d(p)) ##= 50 function from issue #1
+        c = 0
         # while loop from l.7 to 34
         while c < 5
-            global DoubleN = Np
+            DoubleN = Np
             #DoubleN = N(p) #Don't forget this needs to be sorted by clearing error (Issue #6)
             foundnextstep[1] = false
             # repeat from l. 10 to 16
-            global dem = zeros(M)
-            findnextprices!(ptild,  DoubleN, dem, foundnextstep, τ)
-            # while foundnextstep == false | isempty(DoubleN) == false
-            #     e = findingnextstep(DoubleN, τ)
-            #     ptild, DoubleN, dem, foundnextstep = e["ptild"], e["DoubleN"], e["dem"], e["foundnextstep"]
-            #     #println(foundnextstep, ptild) #ptild = good
-            # end
-            println(ptild) #empty - not good
+            #dem = zeros(M)
+            println("l.34 $τ") #not a problem
+            findnextprices!(ptild,  DoubleN, dem, foundnextstep, τ) #definitely here
+            println("l. 36 $τ") #
+            #println(ptild)
+            #println(foundnextstep)
+            #println(τ) #empty - not good
             # if from line 17 to 33
+            #push!(τ,dem)
+            #println(τ)
             if isempty(DoubleN) == true
-                global c = 5
+                #println("Making it to l. 45")
+                c = 5
             end
-            if foundnextstep == true
-                newsetup()
-                global currenterror =  α.(dem)
-                replacesearcherror()
-                replacebesterror()
+            #println("l 50 $τ")
+            if foundnextstep[1] == true
+                #println("Making it to l. 50")
+                newsetupa = newp(ptild)
+                #println("l.54 $dem")
+                push!(τ,copy(dem)) #This needs to be a copy, otherwise you just get a huge array of identical demand vectors
+                #τ[:] = newsetupa["τ"] #This line is not the problem. The problem arises in the push
+                p = newsetupa["p"]
+                currenterror =  α.(dem)
+                replacesearcherror(currenterror, searcherror, c)
+                replacebesterror(currenterror, besterror, p)
             end
+            #println("l.60 $τ")
         end
+
     end
     println("Best price vector is $pstar")
 end
 
-coursematch(foundnextstep)
+M = 5 #Number of courses offered
+k = 3 #Number of courses students take
+βmax = 20 #Maximum budget
+t = 0.005 #time in seconds
+besterror = 100 #for now
+Np = Array[[0,0,0,0,0], [1,1,1,1,1], [1,2,3,0,0]]
+coursematch(M, k, βmax, besterror, t, Np)
+
+# T = Array[]
+# T1 = [0,0,0,0,0]
+# push!(T,T1)
+# T2 = [1,1,1,1,1]
+# push!(T,T2)
+# T3 = [1,2,3,0,0]
+# push!(T,T3)
+# println(T)
