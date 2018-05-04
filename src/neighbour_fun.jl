dc443cc21245605300090f7d1778078e9caf7056
augustindao <augustin.dao@gmail.com>
2018-05-04 10:51:39 +0200

    Updated neighbour function

--- src/neighbour_fun.jl
+++ src/neighbour_fun.jl
@@ -1,57 +1,133 @@
+using JuMP, Gurobi, StatsBase
 
     """
-        neighb_fun(p::Vector, z::Vector, <keyword arguments>)
+        neighb_fun(price::Vector, demand, clear_error, pref, budget, capacity, ind_max::Int=40)
 
     Return K neighbouring price vectors of the current vector p. The set is
     the union of step search along the gradient of errors z and individual
     course price adjustments.
 
-    # Arguments
-    - `s::Float64=1.0` : the step size of the gradient search
-    - `S::Int=12` : the number of steps along gradient
-    - `ind_max::Int` = the maxmimum number of individual price adjustments
+    # Parameters
+    - ind_max = the maxmimum number of individual price adjustments
     """
-    function neighb_fun(p::Vector, z::Vector, s::Float64=1.0, S::Int=12, ind_max::Int=40)
-        return neighbour_vect = [grad_neighb(p, z, s, S); indiv_adjust(p, z, ind_max)]
+    function neighb_fun(price::Vector, demand, clear_error, pref, budget, capacity, ind_max::Int=40)
+
+        #gradient neighbours
+        grad_neighb = grad_descent(price, clear_error)
+
+        #individually adjusted neighbour prices
+        if countnz(clear_error) <= ind_max
+            indiv_neighb = [indiv_adjust(k, price, demand, clear_error, pref, budget, capacity) for k in 1:length(clear_error) if clear_error[k] != 0]
+        else
+            index  = sample(find(clear_error), ind_max, replace=false) # pick 40 random courses to adjust
+            indiv_neighb = [indiv_adjust(k, price, demand, clear_error, pref, budget, capacity) for k in index]
+        end
+
+        return neighbour_vect = [grad_neighb ; indiv_neighb]
     end
 
-    "Return the set of neighbours of price vector p found after performing S
-    steps along the gradient vector."
-    function grad_neighb(p::Vector,z::Vector,step_size::Float64, steps::Int)
-        relat_error = z/maximum(z) #relative clearing error
-        grad_neighb = [p + (step_size * s * relat_error) for s in 1:steps] #for each step price adjusted proportionaly to over/undersub.
+####### Gradient neighbours  #####
+
+    "Return the set of neighbours of price vector p found at different magnitudes
+    along the gradient of error. Increase the price of the most oversubscribed
+    course by magnitude, other course prices adjust relatively ."
+    function grad_descent(price::Vector, clear_error::Vector, steps=[10, 5, 1, 0.5, 0.1])
+        relat_error = clear_error/maximum(clear_error) #relative clearing error
+        grad_neighb = [max(price + (step * relat_error), 0) for step in steps] #price adjusted proportionaly to over/undersub, truncated at zero
         return grad_neighb
     end
 
-    " Return a price vector with adjusted price for course m such that:
-    If m is oversubscribed increase price to decrease error by 1, if m is
-    undersubscribed drop course price to zero."
-    function adjust_rule(m::Int, p::Vector, z::Vector)
-        p_adj = copy(p) #make copy instead of pointer
-        if z[m] < 0
-            p_adj[m] = 0 # if course is undersubscribed, p = 0
-        elseif z[m] > 0
-            p_adj[m] += 1 # MISSING: if course oversub. reduce demand for m by 1
+
+########## Individual adjustment ##########
+
+    """
+    Adjust the price of course m to reduce demand by 1 if the course is oversubscribed
+    or drop the price to zero if the course is undersubscribed.
+    """
+    function indiv_adjust(k::Int, price::Vector, demand, clear_error, pref, budget, capacity)
+        adj_price = copy(price)
+
+        if clear_error[k] < 0
+            adj_price[k] = 0 # if course is undersubscribed, p = 0
+        elseif clear_error[k] > 0
+            adj_price[k] = decrease_demand(k, price, demand, clear_error, pref, budget, capacity)
         end
-        return p_adj
+
+        return adj_price
     end
 
-    "Return the set I of individual-course adjusted price vectors, with an upper bound on I."
-    # Set of individually adjusted price neighbours
-    function indiv_adjust(p::Vector, z::Vector, ind_max::Int=40)
-        if countnz(z) <= ind_max
-            ind_adj_neighb = [adjust_rule(m, p, z) for m in 1:length(z) if z[m] != 0]
-        else
-            # MISSING - pick 40 random courses to adjust ("evenly and randomly assigned")
-            ind_adj_neighb = [adjust_rule(m, p, z) for m in 1:length(z) if z[m] != 0]
+
+    """
+    Return the adjusted price of course m such that its demand decreases by
+    exactly one.
+    """
+    function decrease_demand(k::Int, price::Vector, demand, clear_error, pref, budget, capacity)
+        S = length(pref) #number of students
+        N = length(price) #number of courses
+        min_increase = []
+
+        for i in 1:S
+            if dem["ind_demands"][i][k] == 1 #For all agents i demanding course k
+            # 1) Find agent i's most prefered bundle not including x_m
+                m1 = Model(solver=GurobiSolver())
+                @variable(m1, x[1:N], Bin)
+                @objective(m1, Max, x'*pref[i]*x) #max preferences
+                @constraint(m1, sum(x) == capacity[i]) # course constraint
+                @constraint(m1,  dot(price, x) <= budget[i]) # budget constraint
+                @constraint(m1, x[k] == 0) # in set of bundles without course k
+                status = solve(m1)
+                o = getobjectivevalue(m1) # utility of most prefered bundle not including k
+
+            # 2) Solve for minimum cost bundle in set of feasible solution with objective value greater than o
+                m2 = Model(solver=GurobiSolver())
+                @variable(m2, x[1:N], Bin)
+                @objective(m2, Min, dot(price,x))
+                @constraint(m2, x'*pref[i]*x >= o ) #bundles prefered to o
+                @constraint(m2, sum(x) == capacity[i])
+                @constraint(m2, x[k] == 1) #sechdule inclue course k
+                status_2 = solve(m2)
+                pi_opt = getobjectivevalue(m2) #cost of cheapest bundle including k
+
+            # 3)Price increase for indiv i to drop course k
+                if isnan(pi_opt) == false #drop infeasible results
+                    push!(min_increase, budget[i] - pi_opt + 1) #eps to be scaled to prices
+                end
+            end
         end
-        return ind_adj_neighb
+        return price[k] + minimum(min_increase)
     end
 
-    # TEST - most basic - M=4
-    p = [15.09,5.211,4.37,4.0]
-    z = [0,-5,10,8] #clearing error
-    N = neighb_fun(p,z)
-    # should return a K=12+3=15-element array of 4-element vectors:
-    # - price of course 1 should not change
-    # - price of course 2 should drop to 0 in indiv search
+
+
+    ##### TEST - 20 individuals, 10 classes to choose from, exactly 3 classes to attend
+
+    #setup
+    ind_pref = []
+    for i in 1:20
+        push!(ind_pref, sparse(collect(1:10), collect(1:10), rand(0:100, 10)))
+    end
+
+    budget = rand(150:200, 20)
+    capacity = fill(3, 20)
+    price = rand(30:70, 10)
+    max_spots = rand(5:7, 10)
+
+    dem = demand(price, ind_pref, budget, capacity)
+    clear_error = dem["total_demand"] - max_spots
+
+    # Test functions
+    #grad neighbour
+    grad_descent(price, clear_error)
+    # should return 5 element array (10 element vectors)
+
+    # indiv neighbour
+    #decrease_demand(k, price, dem, clear_error, ind_pref, budget, capacity)
+    k = 2
+    new_price = indiv_adjust(k, price, demand, clear_error, ind_pref, budget, capacity)
+    dem2 = demand(new_price, ind_pref, budget, capacity)
+    clear_error2 = dem2["total_demand"] - max_spots
+    #should find either new_price k = 0 or new clear error drop by 1
+
+    neighb_fun(price, demand, clear_error, ind_pref, budget, capacity)
+
+    #####
